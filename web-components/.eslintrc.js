d424e4ba6d351d8636b65663c187d8217610850a
shavros <shavrin.sb@phystech.edu>
2019-11-24 21:47:25 +0300

    airbnb trying

--- web-components/.eslintrc.js
+++ web-components/.eslintrc.js
@@ -1,3 +1,22 @@
 module.exports = {
-  'extends': "airbnb-base"
-};
+  /*env: {
+    browser: true,
+    es6: true,
+  },*/
+  extends: [
+    'airbnb',
+  ]/*,
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+*/};
