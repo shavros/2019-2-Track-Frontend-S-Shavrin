e341b730573126a9a41eb491b3402dbc205fe47d
shavros <shavrin.sb@phystech.edu>
2019-11-28 02:27:24 +0300

    airbnb

---
+++ web-components/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "globals": {
+    "window": true,
+    "document": true,
+    "HTMLElement": true,
+    "localStorage": true,
+    "customElements": true,
+    "Event": true,
+  },
+};
+
