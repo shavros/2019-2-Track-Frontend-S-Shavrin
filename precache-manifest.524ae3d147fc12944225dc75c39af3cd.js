bd1d28804c1983981211cf74488fd49613b3ca8b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-28 17:42:12 +0000

    Deploy shavros/2019-2-Track-Frontend-S-Shavrin to github.com/shavros/2019-2-Track-Frontend-S-Shavrin.git:gh-pages

---
+++ precache-manifest.524ae3d147fc12944225dc75c39af3cd.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05539c6ae55edf8bdb319e51358957a5",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/index.html"
+  },
+  {
+    "revision": "236512e836fcd7a1db6c",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/css/main.b78f77ae.chunk.css"
+  },
+  {
+    "revision": "568202e4b674c0eea777",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/js/2.5aa64ac4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/js/2.5aa64ac4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "236512e836fcd7a1db6c",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/js/main.a1e6e396.chunk.js"
+  },
+  {
+    "revision": "ba61f4f5738316eb42f9",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/js/runtime-main.8c42553d.js"
+  },
+  {
+    "revision": "799dbb14f5ac900e616ea313f6ed838f",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/media/audiorecordstart_button.799dbb14.svg"
+  },
+  {
+    "revision": "36239a46e9da5b74e3e61489a7f102fc",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/media/audiorecordstop_button.36239a46.svg"
+  },
+  {
+    "revision": "a0a4db7f15d2ade3c1b3cf84ba0ff47f",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/media/backward_button.a0a4db7f.svg"
+  },
+  {
+    "revision": "3664fbdc4bad6ab6ecec21e6b9f61dc2",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/media/companion_logo.3664fbdc.svg"
+  },
+  {
+    "revision": "35407f34de37269cd4c85bb9ded2b5dd",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/media/createchat_button.35407f34.svg"
+  },
+  {
+    "revision": "fd88921a14b204e4b2d8e4ec3851003c",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/media/double_tick.fd88921a.svg"
+  },
+  {
+    "revision": "ce4052ffbe7343b7cc2f71ef4fe2f8e2",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/media/edit_profile_img.ce4052ff.svg"
+  },
+  {
+    "revision": "46967ed61f1f9de869755c77a5acf42b",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/media/geolocation_button.46967ed6.svg"
+  },
+  {
+    "revision": "3520e6624cd6ca47578286f41a1467af",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/media/imgupload_button.3520e662.svg"
+  },
+  {
+    "revision": "b11b5f29421d6f40907f4cec94b4cc8b",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/media/menu_button.b11b5f29.svg"
+  },
+  {
+    "revision": "6608f2b8a55967fd4338c1c9d66012ee",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/media/search_button.6608f2b8.svg"
+  },
+  {
+    "revision": "90709bf03f6b679eeeef8fbb4bf8ba43",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/media/settings_button.90709bf0.svg"
+  },
+  {
+    "revision": "5a87552b4a712cfa721be2b3bf5146df",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/media/submit_button.5a87552b.svg"
+  },
+  {
+    "revision": "f0b9c312e67fc90ee155d1493581b49c",
+    "url": "/2019-2-Track-Frontend-S-Shavrin/static/media/tick.f0b9c312.svg"
+  }
+]);
